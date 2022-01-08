e3f785e6590afa7c8079af3c57f5d559c1959fb7
zak <zak@knill.dev>
2022-01-08 15:55:54 +0000

    feat(payment): accept correct change only
    Update the hopper calculation to complete the purchase when exact
    payment+change can be made.
    
    The hopper calculates how to change for the product and give change,
    using the tray and hopper coins. This could mean giving change from the
    unused coins in the tray, as well as from the hopper.

--- coinage/hopper.go
+++ coinage/hopper.go
@@ -1,5 +1,7 @@
 package coinage
 
+import "fmt"
+
 // Hopper contains the money inside the machine
 // it controls deposits.
 type Hopper struct {
@@ -18,21 +20,146 @@ func NewHopper(changeFloat []uint) *Hopper {
 
 // Deposit the coins from the input tray into the hopper.
 // Coins are checked against the price, and change is returned.
-// Only exact coins are currently accepted, and change will
-// never be given. Deposit will return true if the purchase
-// has been made and empty the tray. Deposit will return false
-// if the purchase has not been made, e.g. not enough money, or
-// change cannot be given for the purchase.
+// Deposit will return true if the purchase has been made and
+// empty the tray. Deposit will return false if the purchase
+// has not been made, e.g. not enough money, or change cannot
+// be given for the purchase.
 func (h *Hopper) Deposit(t *Tray, price uint) ([]uint, bool) {
-	// Exact coins for
-	if price == sumCoins(t.coins) {
-		h.coins = append(h.coins, t.coins...)
-		t.Reset()
+	usedTrayCoins := make([]bool, len(t.coins))
+	usedHopperCoins := make([]bool, len(h.coins))
 
-		return nil, true
+	tSolution, hSolution, solved := resolveCoins(int(price), t.coins, usedTrayCoins, h.coins, usedHopperCoins)
+	if !solved {
+		return nil, false
 	}
 
-	return nil, false
+	var hopper []uint
+	var change []uint
+
+	for i, used := range tSolution {
+		if used {
+			hopper = append(hopper, t.coins[i])
+		} else {
+			change = append(change, t.coins[i])
+		}
+	}
+
+	for i, used := range hSolution {
+		if used {
+			change = append(change, h.coins[i])
+		} else {
+			hopper = append(hopper, h.coins[i])
+		}
+	}
+
+	fmt.Printf("hopper: %v", hopper)
+	fmt.Printf("change: %v", change)
+
+	t.coins = make([]uint, 0)
+	h.coins = hopper
+
+	return change, true
+}
+
+// We have coins in the tray
+// we have coins in the hopper
+// we want to pay for the item, and return change.
+// ideally this nets-out to zero.
+// +ve number is overpay (currently, not acceptable)
+// -ve number is over-change (underpay, not acceptable)
+// 0 number is net-out
+
+// search tree problem
+// what set of coins on left (tray) and right (hopper)
+// can be used to net out to zero
+
+// find the coins that meet or exceed the price
+//  - if meets exactly --> DONE
+//  - if overpay, find the coins from the hopper that we can use to net out.
+// best solution is one that minimises overpayment
+// (config for if overpayment is allowed)
+
+// returns []usedTrayCoins, []usedHopperCoinsForChange, matched
+func resolveCoins(
+	price int,
+	trayCoins []uint,
+	usedTrayCoins []bool,
+	hopperCoins []uint,
+	usedHopperCoins []bool,
+) ([]bool, []bool, bool) {
+
+	fmt.Printf("---\nprice: %d\ntray: %v\nhopper: %v\n", price, usedTrayCoins, usedHopperCoins)
+
+	if price == 0 {
+		return usedTrayCoins, usedHopperCoins, true
+	}
+
+	if price > 0 {
+		// not met
+		// try and make price from tray
+
+		for i := range usedTrayCoins {
+			if usedTrayCoins[i] {
+				continue
+			}
+
+			coin := trayCoins[i]
+			remainingPrice := price - int(coin)
+
+			updatedUsed := markIndex(usedTrayCoins, i)
+
+			traySolution, hopperSolution, solved := resolveCoins(
+				remainingPrice,
+				trayCoins,
+				updatedUsed,
+				hopperCoins,
+				usedHopperCoins,
+			)
+
+			if solved {
+				return traySolution, hopperSolution, true
+			}
+		}
+	}
+
+	if price < 0 {
+		// currently overpaying
+		// try and give change from hopper
+
+		for i := range usedHopperCoins {
+			if usedHopperCoins[i] {
+				continue
+			}
+
+			coin := hopperCoins[i]
+			remainingPrice := price + int(coin)
+
+			updatedUsed := markIndex(usedHopperCoins, i)
+
+			traySolution, hopperSolution, solved := resolveCoins(
+				remainingPrice,
+				trayCoins,
+				usedTrayCoins,
+				hopperCoins,
+				updatedUsed,
+			)
+
+			if solved {
+				return traySolution, hopperSolution, true
+			}
+		}
+	}
+
+	return nil, nil, false
+}
+
+func markIndex(in []bool, idx int) []bool {
+	out := make([]bool, len(in))
+	copy(out, in)
+
+	out[idx] = true
+
+	return out
 }
 
 func (h *Hopper) Value() uint {
--- coinage/hopper_test.go
+++ coinage/hopper_test.go
@@ -11,12 +11,13 @@ var _ = Describe("Hopper", func() {
 	d := coinage.NewDenominations(1, 5, 10, 25, 50, 100)
 
 	Context("Depositing correct money", func() {
-		tray := coinage.NewTray(d)
-		var hopper *coinage.Hopper
+		var (
+			hopper *coinage.Hopper
+			tray   *coinage.Tray
+		)
 
 		BeforeEach(func() {
-			tray.Reset()
-
+			tray = coinage.NewTray(d)
 			hopper = coinage.NewHopper(nil)
 		})
 
@@ -41,6 +42,63 @@ var _ = Describe("Hopper", func() {
 			Expect(tray.MeetsPrice(0)).To(BeTrue())
 
 			Expect(hopper.Value()).To(Equal(uint(31)))
+		})
+
+		It("succeeds with extra money", func() {
+			Expect(hopper.Value()).To(BeZero())
+
+			tray.Insert(1, 1, 25, 5)
+
+			change, ok := hopper.Deposit(tray, 31)
+
+			Expect(ok).To(BeTrue())
+			Expect(change).To(BeEquivalentTo([]uint{1}))
+
+			Expect(tray.MeetsPrice(1)).To(BeFalse())
+			Expect(tray.MeetsPrice(0)).To(BeTrue())
+
+			Expect(hopper.Value()).To(Equal(uint(31)))
+		})
+
+		It("succeeds with change from hopper", func() {
+			float := make([]uint, 99)
+			expectedChange := make([]uint, 99)
+			for i := range float {
+				float[i] = 1
+				expectedChange[i] = 1
+			}
+			hopper = coinage.NewHopper(float)
+
+			tray.Insert(100)
+
+			change, ok := hopper.Deposit(tray, 1)
+
+			Expect(ok).To(BeTrue())
+			Expect(change).To(BeEquivalentTo(expectedChange))
+
+			Expect(tray.MeetsPrice(1)).To(BeFalse())
+			Expect(tray.MeetsPrice(0)).To(BeTrue())
+
+			Expect(hopper.Value()).To(Equal(uint(100)))
+		})
+	})
+
+	Context("Overpaying", func() {
+		var (
+			hopper *coinage.Hopper
+			tray   *coinage.Tray
+		)
+
+		BeforeEach(func() {
+			tray = coinage.NewTray(d)
+			hopper = coinage.NewHopper(nil)
+		})
+
+		It("fails when change cannot be given", func() {
+			tray.Insert(25)
+
+			_, ok := hopper.Deposit(tray, 24)
+			Expect(ok).To(BeFalse())
 		})
 	})
 })
